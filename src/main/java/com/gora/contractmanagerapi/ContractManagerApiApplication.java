b547f9f407bd68e84dc3a54d79a367e1a6f267cf
SrBruninho <bos.bruno.oliveira55@gmail.com>
2025-10-06 12:18:10 -0300

    CMA-001 - Initial Commit

---
+++ src/main/java/com/gora/contractmanagerapi/ContractManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.gora.contractmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gora/contractmanagerapi/ContractManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gora.contractmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
